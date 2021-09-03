767016f0df55566e4bccfe874a2446ba2222cdce
Luciano-Fuertes <chiva.fuertes@gmail.com>
2021-09-03 10:44:41 -0300

    algo de progreso

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,36 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    highlights: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    score: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    healthyLevel: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    cookingSteps: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const axios = require('axios');
+const {Recipe, Diet} = require('../db')
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+
 
 
 module.exports = router;
