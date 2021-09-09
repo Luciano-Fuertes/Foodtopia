6ba8e5ad38a82fe7f8b51f32ecc5355eb9e7197d
Luciano-Fuertes <chiva.fuertes@gmail.com>
2021-09-09 15:35:11 -0300

    creo que paginado

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRecipe } from '../actions';
 import { Link } from 'react-router-dom'
 import Card from './Card';
 import NavBar from './NavBar';
+import Pages from './Pages';
 
 function Home() {
 
     const dispatch = useDispatch();
     const allRecipes = useSelector((state) => state.recipes)
+    const [currentPage, setCurrentPage] = useState(1);
+    const [recipesPerPage, setRecipesPerPage] = useState(9);
+    const indexLast = currentPage * recipesPerPage;
+    const indexFirst = indexLast - recipesPerPage;
+    const currentRecipe = allRecipes.slice(indexFirst, indexLast);
+
+    const pageHandler = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    }
+
 
     useEffect(() => {
         dispatch(getRecipe())
@@ -22,7 +33,7 @@ function Home() {
 
     return (
         <div>
-            <NavBar className="Nav"/>
+            <NavBar className="Nav" />
             <Link to='/recipe'>Submit New Recipe</Link>
             <button onClick={e => { handleClick(e) }}>
                 Show All Recipes
@@ -37,11 +48,16 @@ function Home() {
                     <option value='existent'>Existent</option>
                     <option value='created'>Created</option>
                 </select>
-                {allRecipes?.map((c) => {
+                <Pages
+                    recipesPerPage={recipesPerPage}
+                    allRecipes={allRecipes.length}
+                    pageHandler={pageHandler}
+                />
+                {currentRecipe?.map((c) => {
                     return (
                         <div>
                             <Link to={'/home/' + c.id}>
-                                <Card name={c.name} diet={c.diet} image={c.image} />
+                                <Card name={c.name} diet={c.diet} image={c.image} key={c.id} />
                             </Link>
                         </div>
                     )
---
+++ client/src/components/Pages.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Pages({ recipesPerPage, allRecipes, pageHandler }) {
+    const pageNumbers = []
+
+    for (let i = 0; i <= Math.ceil(allRecipes / recipesPerPage); i++) {
+        pageNumbers.push(i+1)
+    }
+
+    return (
+        <nav>
+            <ul>
+                {pageNumbers?.map((num) => {
+                    <li className="number" key={num}>
+                        <a onClick={() => pageHandler(num)}>{num}</a>
+                    </li>
+                })}
+            </ul>
+        </nav>
+    )
+}
+
+export default Pages
